e93d86088ee51fccb5c008be38239b731e9dadfd
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-31 14:32:54 -0300

    Feat: added the footer file

---
+++ pages/php/partials/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./partials_css.css">
+	</head>
+	<body>
+		<footer>
+				
+		</footer>
+	</body>
+</html>
