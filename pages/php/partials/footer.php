6e31b1af2e516eef04776a9b841c8e3cc542008c
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-31 14:44:51 -0300

    Updated the footer

--- pages/php/partials/footer.php
+++ pages/php/partials/footer.php
@@ -8,7 +8,18 @@
 	</head>
 	<body>
 		<footer>
-				
+			<div>
+				<div>
+					<img src="logo" alt="imagem da logo">
+					<p>Todos os direitos reservados à BodyCare</p>
+				</div>
+				<div>
+					<h2>CONTATO</h2>
+					<p>Rua justino paixão 123</p>
+					<p>+55 11 93056-9806</p>
+					<p>BodyCareBusiness@gmail.com</p>		
+				</div>
+			</div>
 		</footer>
 	</body>
 </html>
